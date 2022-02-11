9686328a3b1a04563740148ea6fce4b636a6826c
hasnatulalam <hasnatualam056@gmail.com>
2022-02-11 12:14:59 +0600

    f

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+
+
+
+
+
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [search, setSearch] = useState("")
+
+
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data)
+
+      })
+  }, [users])
+  // Get current posts
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input placeholder='Search' type="text" onChange={(event) => {
+        setSearch(event.target.value)
+      }}
+      />
+      {
+        users.filter((val) => {
+          if (search == "") {
+            return val
+          } else if ((val.name.toLowerCase().includes(search.toLowerCase())) || (val.username.toLowerCase().includes(search.toLowerCase()))) {
+            return val
+          }
+
+        }).map((val, key) => {
+          return (
+
+            <div className='user' key={key}>
+              <p>{val.name}</p>
+              <p>val.username</p>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          );
+        })}
+
+
     </div>
+
+
+
+
+
   );
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
